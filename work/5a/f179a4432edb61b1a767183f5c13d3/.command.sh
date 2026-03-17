6c7429b28431d943fcfd5f50b3f8cdca19feab99
Mihir Chachar <mchachar143@gmail.com>
2026-03-17 00:44:56 +0000

    removing large data files and adding gitignore text file

--- work/5a/f179a4432edb61b1a767183f5c13d3/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-bwa-mem2 mem -t 4 LG12.fasta A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_1.trimmed.fq.gz A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub_2.trimmed.fq.gz | samtools sort -@ 4 -o A_33_FDSW202661760-1r_HTNK5DSXY_L3_sub.bam
